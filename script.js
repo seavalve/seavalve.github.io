1f201039549df7d507230fc16e9a4e861cfbc44e
seavalve <seavalve0@gmail.com>
2026-04-16 01:12:13 +0530

    Add files via upload
    added responsiveness cuz mobile was being a bitch as usual

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-spo
+// funny story i WAS planning on making this a feature rich website with audio, and much more interactable stuff but then
+// i decided not to cuz ill do all that in a future website so uhm yeah 
+// "wHy nOt dElEtE tHe fIlE tHEn?" i hear you ask me in your annoying bitchy little voice and i kept it here cuz it looks cool alright? sue me
+// also WHY THE FUCK ARE YOU EVEN HERE
